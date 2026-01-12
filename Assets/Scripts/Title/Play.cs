e603aeaa02a95f05c8c9333e286fcea13a4a9070
mandugii <dbalsgur12345@gmail.com>
2026-01-12 23:29:37 +0900

    Made Prolog Map

---
+++ Assets/Scripts/Title/Play.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+public class Play : MonoBehaviour
+{
+    public GameObject panel;
+    private CanvasGroup panelGroup;
+    public float fadeTime = 2f;
+    
+    public void ClickOnPlay()
+    {
+        panel.SetActive(true);
+        StartCoroutine(fadeOut());
+    }
+    IEnumerator fadeOut()
+    {
+        panelGroup = panel.GetComponent<CanvasGroup>();
+        float currentTime = 0f;
+        while(currentTime < 1f)
+        {
+            currentTime += Time.deltaTime / fadeTime;
+            panelGroup.alpha=Mathf.Lerp(0,1,currentTime);
+            yield return null;
+        }
+        panelGroup.alpha = 1;
+    }
+}
